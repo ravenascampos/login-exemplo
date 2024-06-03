de0f39546b9743d81758981a22b4367b160b039c
RAVENA CAMPOS <ravenascampos@gmail.com>
2024-06-03 18:15:49 -0300

    header

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,19 +1,35 @@
-import { Button } from "@mui/material"
+import { AppBar, Button, Container, Toolbar } from "@mui/material"
 import { Link as RouterLink } from 'react-router-dom';
 
 function HomePage() {
     return (
-        <Button
-            //o LinkComponent recebe o RouterLink do react-router-dom e o to indica a rota que vc quer 
-            LinkComponent={RouterLink}
-            to="/login"
-            sx={{
-                backgroundColor: "#DA0175",
-                color: "#F8F8F9",
-                width: '160px',
-                marginBottom: '20px'
-            }}
-        >Login</Button>
+        <>
+            <AppBar            >
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <Button
+                            LinkComponent={RouterLink}
+                            to="/login"
+                            variant="text" color="inherit"
+                        >Login</Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+
+
+            <Button
+                //o LinkComponent recebe o RouterLink do react-router-dom e o to indica a rota que vc quer 
+                LinkComponent={RouterLink}
+                to="/login"
+                sx={{
+                    backgroundColor: "#DA0175",
+                    color: "#F8F8F9",
+                    width: '160px',
+                    marginBottom: '20px',
+                    marginTop: '5rem'
+                }}
+            >Login</Button>
+        </>
     )
 }
 
